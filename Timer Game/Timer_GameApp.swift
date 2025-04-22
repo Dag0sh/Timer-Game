d7d73270c5776fbe87d62a02168559ad9ce4cd2b
Dagosh <danilreal25@gmail.com>
2025-04-22 20:19:33 +0300

    Initial Commit

---
+++ Timer Game/Timer_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Timer_GameApp.swift
+//  Timer Game
+//
+//  Created by Dagosh on 22.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct Timer_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Timer GameTests/Timer_GameTests.swift
@@ -0,0 +1,17 @@
+//
+//  Timer_GameTests.swift
+//  Timer GameTests
+//
+//  Created by Dagosh on 22.04.2025.
+//
+
+import Testing
+@testable import Timer_Game
+
+struct Timer_GameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
